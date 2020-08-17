1b65852a2b54c6c99d48c1fda8c8642ea5776988
gitihm <s5935521030@phuket.psu.ac.th>
2020-08-17 16:17:32 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.css'
+import axios from 'axios'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [allBlob, setAllBlob] = React.useState([])
+	const setFile = (e) => {
+		preCompresss(e.target.files)
+	}
+	const compresss = (file, type, newWidth, quality) => {
+		const reader = new FileReader()
+		reader.readAsDataURL(file)
+		reader.onload = (event) => {
+			const img = new Image()
+			img.src = event.target.result
+			img.onload = () => {
+				const elem = document.createElement('canvas')
+				elem.width = newWidth
+				elem.height = (img.height / img.width) * newWidth
+				const ctx = elem.getContext('2d')
+				ctx.drawImage(img, 0, 0, elem.width, elem.height)
+				ctx.canvas.toBlob(
+					(blob) => {
+						setAllBlob((allBlob) => [...allBlob, blob])
+					},
+					type,
+					quality
+				)
+			}
+			reader.onerror = (error) => console.log(error)
+		}
+	}
+
+	const preCompresss = (files) => {
+		for (const key in files) {
+			if (files.hasOwnProperty(key)) {
+				const file = files[key]
+				console.log(file.size)
+				compresss(file, 'image/jpeg', 1080, 0.95)
+			}
+		}
+	}
+	const submit = async (e) => {
+		e.preventDefault()
+		var formDataToUpload = new FormData()
+		for (const iterator of allBlob) {
+			formDataToUpload.append('img[]', iterator)
+		}
+		let res = await axios({
+			method: 'post',
+			url: 'http://localhost:8000/upload',
+			data: formDataToUpload,
+			headers: { 'Content-Type': 'multipart/form-data' },
+		})
+		alert(res.data.msg)
+	}
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<div>
+					<form onSubmit={submit}>
+						<input type='file' onChange={setFile} multiple />
+						<input type='submit' value='upload' />
+					</form>
+				</div>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
